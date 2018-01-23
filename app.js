1da9e3d7ac06014e75e7b686d9577bbf08eaa379
captain-c00keys <gpieterson.wsu@hotmail.com>
2018-01-23 10:13:35 -0800

    added app.js functionality

--- app.js
+++ app.js
@@ -2,19 +2,23 @@
 
 pageLoad();
 
+var __API_URL__ = 'https://mg-book-app.herokuapp.com';
+
 $('#basic-form').on('submit', function(e){
     e.preventDefault();
 
-    $.post('/db/person', data)
+    $.post(`${__API_URL__}/db/person`, data)
     .then(function () {
-        pageLoad
+        pageLoad();
     })
+    .catch(function() {
+        pageLoad();
+    });
 });
 
 function pageLoad() {
-    $.get('/db/person')
+    $.get(`${__API_URL__}/db/person`)
     .then(function(data) {
        $('#results').empty();
-
     });
 }
