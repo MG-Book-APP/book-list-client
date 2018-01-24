16d09783c620c241c197be84bd1bed8e81087da2
Melanie Cohen <melaniebcohen@gmail.com>
2018-01-23 18:30:47 -0800

    changes

--- app.js
+++ app.js
@@ -4,30 +4,33 @@ pageLoad();
 
 var __API_URL__ = 'https://mg-book-app.herokuapp.com';
 
-$('#book-form').on('submit', function(e){
+$('#book-form').on('submit', function(e) {
   e.preventDefault();
 
   let data = {
     // capture data from form, create data object
-    title: e.target.title.value,
     author: e.target.author.value,
-    url: e.target.url.value,
+    title: e.target.title.value,
+    isbn: e.target.isbn.value,
+    image_url: e.target.image_url.value,
+    description: e.target.description.value,
   }
 
-  // AJAX call to /books?
-  $.post(`${__API_URL__}/db/books`, data)
-    .then(function () {
+  // do AJAX call here if it's confirmed that post request worked
+  $.post(`${__API_URL__}/api/v1/books`, data)
+    .then(function() {
       pageLoad();
     })
     .catch(function() {
       pageLoad();
     })
+  this.reset();
 });
 
-// Load JSON file into DB
-$.get('/', function(req,res) {
-  res.sendFile('./data/books.json')
-})
+// // Load JSON file into DB
+// $.get('/', function(req,res) {
+//   res.sendFile('./data/books.json')
+// })
 
 function pageLoad() {
   $.get(`https://mg-book-app.herokuapp.com/api/v1/books`)
