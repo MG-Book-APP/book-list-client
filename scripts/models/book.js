8f1fff71142c2d6ae902674377a1c795927a37e2
Melanie Cohen <melaniebcohen@gmail.com>
2018-01-24 17:36:54 -0800

    error view update

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -15,17 +15,28 @@
 
   // load All
   Book.loadAll = rawData => {
+    // *TO DO*: SORT ROWS BY TITLE 
     rawData.forEach(bookObject => Book.all.push(new Book(bookObject)))
   }
 
-  // fetch from JSON
-  // in the future this should be a get request to the server/db
   Book.fetchAll = () => {
-    $.getJSON('./data/books.json')
-      .then(rawData => {
-        Book.loadAll(rawData)
+    $.get('https://mg-book-app.herokuapp.com/api/v2/books')
+      .then(function(data) {
+        $('#results').empty();
+        data.rows.forEach(function(book) {
+          let content = `
+            <p>author: ${book.author}</p>
+            <h2>title: ${book.title}</h2>
+            <p>ISBN: ${book.isbn}</p>
+            <p>URL: ${book.image_url}</p>
+            <p>Description: ${book.description}</p>
+            `
+          $('#results').append(content);
+        })
+      }, err => {
+        errorView.initErrorPage(err);
       })
-  }
+  };
 
   // insert record
   Book.prototype.insertRecord = function(callback) {
@@ -38,6 +49,36 @@
       })
       .then(callback)
   }
+
+  // Form - add book
+  $('#book-form').on('submit', function(e) {
+    e.preventDefault();
+  
+    let data = {
+      // capture data from form, create data object
+      author: e.target.author.value,
+      title: e.target.title.value,
+      isbn: e.target.isbn.value,
+      image_url: e.target.image_url.value,
+      description: e.target.description.value,
+    }
+  
+    $.get('https://mg-book-app.herokuapp.com/api/v1/books')
+      .then(function(data) {
+        $('.container').sort();
+        console.log(data);
+      })
+  
+    // do AJAX call here if it's confirmed that post request worked
+    $.post(`${__API_URL__}/api/v1/books`, data)
+      .then(function() {
+        pageLoad();
+      })
+      .catch(function() {
+        pageLoad();
+      })
+    this.reset();
+  });
 
   Book.fetchAll();
   module.Book = Book;
