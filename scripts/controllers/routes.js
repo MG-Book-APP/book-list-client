cad0e46fe87a38d7867908e28b7e799a1f5a875f
Melanie Cohen <melaniebcohen@gmail.com>
2018-01-28 11:44:25 -0800

    routing work

--- scripts/controllers/routes.js
+++ scripts/controllers/routes.js
@@ -1,6 +1,9 @@
 'use strict';
 
-page('*', window.Book.fetchAll, window.Book.loadAll, window.Book.renderAllBooks, window.bookView.init);
+page('/book-app-client', function() { 
+  page.redirect('/');
+});
+page('/', window.Book.fetchAll, window.Book.loadAll, window.Book.renderAllBooks, window.bookView.init);
 page('/books/add', window.formView.init);
 page('/books/:id', window.Book.fetchSingle, window.Book.loadSingle, window.Book.renderSingleBook, window.singleBook.init,window.Book.deleteBook);
 
