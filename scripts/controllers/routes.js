540d6597fb1bc90cb32392c97ab2a6be307652ec
Melanie Cohen <melaniebcohen@gmail.com>
2018-01-28 11:22:16 -0800

    testing something

--- scripts/controllers/routes.js
+++ scripts/controllers/routes.js
@@ -1,6 +1,6 @@
 'use strict';
 
-page('/', window.Book.fetchAll, window.Book.loadAll, window.Book.renderAllBooks, window.bookView.init);
+page('*', window.Book.fetchAll, window.Book.loadAll, window.Book.renderAllBooks, window.bookView.init);
 page('/books/add', window.formView.init);
 page('/books/:id', window.Book.fetchSingle, window.Book.loadSingle, window.Book.renderSingleBook, window.singleBook.init,window.Book.deleteBook);
 
